9a614dd7723357a1ce3eb2a988ce7b7bc3cae490
odrin7 <sz.danix6@gmail.com>
2017-03-24 10:41:38 +0100

    histrix dashboardWork

--- bga-monitoring/src/main/java/hu/odrin7/bga/MonitoringApplication.java
+++ bga-monitoring/src/main/java/hu/odrin7/bga/MonitoringApplication.java
@@ -2,16 +2,12 @@ package hu.odrin7.bga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
-/**
- * Created by Tarun Kumar Sukhu
- */
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableAdminServer
+@EnableTurbineStream
+@EnableHystrixDashboard
 public class MonitoringApplication {
 
     public static void main(String[] args) {
