ca5f8ac6f672f7a2459828fd729d7cb5a4689675
odrin7 <sz.danix6@gmail.com>
2017-03-24 11:55:26 +0100

    remove histrix dashboard

--- bga-monitoring/src/main/java/hu/odrin7/bga/MonitoringApplication.java
+++ bga-monitoring/src/main/java/hu/odrin7/bga/MonitoringApplication.java
@@ -1,13 +1,13 @@
 package hu.odrin7.bga;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableTurbineStream
-@EnableHystrixDashboard
+@EnableDiscoveryClient
+@EnableAdminServer
 public class MonitoringApplication {
 
     public static void main(String[] args) {
